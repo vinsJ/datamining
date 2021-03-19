158dc9a004801500f44a1f45d525fffabb08fa22
Vincent DEBANDE <vincent.debande@devinci.fr>
2021-03-19 20:21:37 +0100

    :construction: Zoom on Paris | Add train cursor

--- geo/geoApp/views.py
+++ geo/geoApp/views.py
@@ -4,19 +4,14 @@ import folium
 
 # Create your views here.
 def home(request):
-    shp_dir = os.path.join(os.getcwd(),'media','shp')
 
     # folium
-    m = folium.Map(location=[46.227638,2.213749],zoom_start=6)
+    # Center of France : [46.227638,2.213749]
 
-    ## style
-    style_basin = {'fillColor': '#228B22', 'color': '#228B22'}
-    style_rivers = { 'color': 'blue'}
+    m = folium.Map(location=[48.866669,  2.33333],zoom_start=12)
+    folium.Marker([48.8763,  2.3254], popup  = "Paris Saint Lazare", icon=folium.Icon(color='red', icon='train', prefix='fa')).add_to(m)
 
     ## adding to view
-    folium.GeoJson(os.path.join(shp_dir,'basin.geojson'),name='basin',style_function=lambda x:style_basin).add_to(m)
-    folium.GeoJson(os.path.join(shp_dir,'rivers.geojson'),name='rivers',style_function=lambda x:style_rivers).add_to(m)
-    folium.LayerControl().add_to(m)
 
     ## exporting
     m=m._repr_html_()
